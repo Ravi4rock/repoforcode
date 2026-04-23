adc9fc848f60b8262aa2f9ca4e6506dbe095b6ac
Ravi4rock <raviranjank97@gmail.com>
2026-04-23 19:30:35 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch41 team with  Chirag, Aniket , Sarang, Sairam");
+     System.out.println("Hello batch41 team with Ravi , Aryan , Sonia, Sreeram");
       System.out.println("Hello Batch batch43 Ravi babu");
     }  
 } 
