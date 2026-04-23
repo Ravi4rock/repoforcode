b5c19f782f1bfff0cc57a6420bdbd8a7903f0548
Ravi4rock <raviranjank97@gmail.com>
2026-04-23 08:51:10 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello batch41 team with  Chirag, Aniket , Sarang, Sairam");
+      System.out.println("Hello Batch batch40 WITH FOR STUDENTS RUNNING in front of me and Santhosh , naveen, komal in making CI automated vikas teach devops CI proecss");
+    }  
+} 
